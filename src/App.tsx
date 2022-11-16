2fb8e981171f707bab251447a7bd74f7af0455cd
Hristian Kanev <hristian.kanev@flatrocktech.com>
2022-11-16 14:02:00 +0200

    note form

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,84 @@
-import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Routes, Route, Navigate } from "react-router-dom";
+import { Container } from "react-bootstrap";
+import { NewNote } from "./NewNote";
+import { useLocalStorage } from "./useLocalStorage";
+import { useMemo } from "react";
+import { v4 as uuidV4 } from "uuid";
+
+export type RawNote = {
+  id: string;
+} & RawNoteData;
+
+export type RawNoteData = {
+  title: string;
+  markdown: string;
+  tagIds: string[];
+};
+
+export type Note = {
+  id: string;
+} & NoteData;
+
+export type NoteData = {
+  title: string;
+  markdown: string;
+  tags: Tag[];
+};
+
+export type Tag = {
+  id: String;
+  label: string;
+};
 
 function App() {
+  const [notes, setnotes] = useLocalStorage<RawNote[]>("NOTES", []);
+  const [tags, setTags] = useLocalStorage<Tag[]>("TAGS", []);
+
+  const notesWithTags = useMemo(() => {
+    return notes.map((note) => {
+      return {
+        ...note,
+        tags: tags.filter((tag) => note.tagIds.includes(tag.id)),
+      };
+    });
+  }, [notes, tags]);
+
+  function onCreateNote({ tags, ...data }: NoteData) {
+    setnotes((prevNotes) => {
+      return [
+        ...prevNotes,
+        { ...data, id: uuidV4(), tagIds: tags.map((tag) => tag.id) },
+      ];
+    });
+  }
+
+  function addTag(tag: Tag) {
+    setTags((prev) => [...prev, tag]);
+  }
+
   return (
-    <div className="App">
-      <h2>React app :P</h2>
-    </div>
-  )
+    <Container className="my-4">
+      <Routes>
+        <Route path="/" element={<h1>Home</h1>}></Route>
+        <Route
+          path="/new"
+          element={
+            <NewNote
+              onSubmit={onCreateNote}
+              onAddTag={addTag}
+              availableTags={tags}
+            />
+          }
+        ></Route>
+        <Route path="/:id">
+          <Route index element={<h1>Show</h1>} />
+          <Route path="edit" element={<h1>Edit</h1>} />
+        </Route>
+        <Route path="*" element={<Navigate to="/" />}></Route>
+      </Routes>
+    </Container>
+  );
 }
 
-export default App
+export default App;
